09d3fe3de378341e9659f7af0cfbb49f26d5aa6f
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2026-03-08 13:53:18 -0400

    Add files via upload

---
+++ src/ReverseString.java
@@ -0,0 +1,45 @@
+public class ReverseString {
+
+    static String reverseString(String s){
+        if(s.length() > 0){
+            return s.substring(s.length()-1) + reverseString(s.substring(0, s.length()-1)) ;
+        }
+
+        return "";
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("===== Testing reverseString =====\n");
+
+        String test1 = "hello";
+        System.out.println("Input: " + test1);
+        System.out.println("Expected: olleh");
+        System.out.println("Actual:   " + reverseString(test1));
+        System.out.println();
+    
+        String test2 = "recursion";
+        System.out.println("Input: " + test2);
+        System.out.println("Expected: noisrucer");
+        System.out.println("Actual:   " + reverseString(test2));
+        System.out.println();
+    
+        String test3 = "a";
+        System.out.println("Input: " + test3);
+        System.out.println("Expected: a");
+        System.out.println("Actual:   " + reverseString(test3));
+        System.out.println();
+    
+        String test4 = "";
+        System.out.println("Input: (empty string)");
+        System.out.println("Expected: ");
+        System.out.println("Actual:   " + reverseString(test4));
+        System.out.println();
+    
+        String test5 = "Hello World";
+        System.out.println("Input: " + test5);
+        System.out.println("Expected: dlroW olleH");
+        System.out.println("Actual:   " + reverseString(test5));
+        System.out.println();
+    }
+}
